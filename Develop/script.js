08ef7f79a7f786f173790d03adf1d2868a9045f1
EvanSterlingMiller <evan.s.miller3@gmail.com>
2023-02-27 05:54:39 -0500

    js code

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,68 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+// this function wraps the code so that the function waits until the page is loaded to execute the code. Source (https://stackoverflow.com/questions/30894981/how-important-it-is-to-wrap-jquery-code-in-document-ready-function)
+
+$(document).ready(function () {
+  var time_now = dayjs()
+  $("#currentDay").text(time_now.format("MMM DD, YYYY hh:mm a")) //  set the time to reflect am/pm
+  // TODO: save button
+  // this is the save button funtion
+  $(".saveBtn").on("click", function() {
+    // var for the change in the discription attribute
+    var save_text = $(this).siblings(".decscription").string()
+    // var for the change in the id attribute
+    var save_time = $(this).parent().attr("id")
+
+    // saving the function to local storage
+    localStorage.setItem(save_time, save_text)
+
+  }) // end of the save button function
+
+  // TODO: past, present future
+  // setting current day information
+  
+  
+  function hour_tracker() {
+      //get current number of hours.
+      var current_hour = dayjs().hour(); 
+
+      // loop over time blocks
+      $(".time-block").each(function () {
+          var hour_block = parseInt($(this).attr("id").split("hour")[1]);
+          
+          if (hour_block < current_hour) {
+              $(this).addClass("past");
+              $(this).removeClass("future");
+              $(this).removeClass("present");
+          }else if (hour_blockr == current_hour) {
+              $(this).removeClass("past");
+              $(this).addClass("present");
+              $(this).removeClass("future");
+          }else {
+              $(this).removeClass("present");
+              $(this).removeClass("past");
+              $(this).addClass("future");
+          }// ends if section
+      })//ends .time-block function
+  }// ends hour_tracker function
+  hour_tracker()
+
+
+  $("#hour-7 .description").val(localStorage.getItem("hour-7"))
+  $("#hour-8 .description").val(localStorage.getItem("hour-8"))
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+  $("#hour-18 .description").val(localStorage.getItem("hour-18"))
+  $("#hour-19 .description").val(localStorage.getItem("hour-19"))
+  $("#hour-20 .description").val(localStorage.getItem("hour-20"))
+
+
+
+})// ends (doucment).ready section, Javascript END
+
+
